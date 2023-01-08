dee05bcd449da8a37dd47a5b8bdfec0eee5dd0ca
fadlyas07 <mhmmdfdlyas@gmail.com>
2023-01-08 13:29:26 +0700

    breeee
    Change-Id: I4912a5241c2fe87123ffb133341c1134b9f4428b

--- build.sh
+++ build.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env bash
-#
 # Copyright (C) 2022 fadlyas07 <mhmmdfdlyas@gmail.com>
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 
 # Set a directory
 DIR="$(pwd ...)"
@@ -84,6 +70,7 @@ rel_friendly_date="$(date '+%B %-d, %Y')" # "Month day, year" format
 pushd $(pwd)/llvm-project
 commit_msg=$(git log --pretty="format:%s" | head -n1)
 short_llvm_commit="$(git rev-parse --short HEAD)"
+
 popd
 llvm_commit_url="https://github.com/llvm/llvm-project/commit/$short_llvm_commit"
 
@@ -107,8 +94,7 @@ Binutils version: $binutils_version
 LLVM repo commit: $commit_msg
 Link: $llvm_commit_url
 
-Releases:
-    https://github.com/greenforce-project/clang-llvm/releases/download/${rel_date}/${files}
+Releases: https://github.com/greenforce-project/clang-llvm/releases/download/${rel_date}/${files}
 
 ")
 git init
